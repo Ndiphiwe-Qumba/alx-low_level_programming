f3381b9c9e88062d27d960c7466a06fffe0f28a1
Ndiphiwe-Qumba <Ndiphiwequmba.nq@gmail.com>
2022-09-12 15:20:47 -0700

    $msg ....Update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num;
+	char pos;
+
+	for (num = '0'; num < '9'; num++)
+	{
+		for (pos = num + 1; pos <= 9; pos++)
+		{
+			if(pos != num)
+			{
+				putchar(num);
+				putchar(pos);
+
+				if(num == '8' && pos == '9')
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	
+	return (0);
+}
