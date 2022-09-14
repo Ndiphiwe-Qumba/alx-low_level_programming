fcdf644adc764ad19568fc32a522cb1cfb416f89
Ndiphiwe-Qumba <Ndiphiwequmba.nq@gmail.com>
2022-09-14 15:39:17 -0700

    $msg ....Update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10.c - prints alphabets 10x
- *
- * Return : null Success
- */
+* print_alphabet_x10 - Entry point, prints letters of alphabet
+* Return: Always null (Success)
+*/
 void print_alphabet_x10(void)
 {
 	char alphabet;
 	int numberoftimes = 10;
 	int n;
 
-	for (n = 0; n <= numberoftimes; n++)
+	for (n = 0; n < numberoftimes; n++)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
