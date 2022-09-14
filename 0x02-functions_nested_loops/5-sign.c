7c7d79af5e1998420d9d953c24364fb2d7b9d043
Ndiphiwe-Qumba <Ndiphiwequmba.nq@gmail.com>
2022-09-14 16:54:07 -0700

    $msg ....Update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of value
+ * @n: is the valllue
+ * Return 1 if positive number
+ * print -1 if neg
+ */
+int print_sign(int n)
+{
+	int val;
+
+	if (n > 0)
+	{
+		val = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		val = 0;
+		_putchar('0');
+	}
+	else
+	{
+		val = -1;
+		_putchar('-');
+	}
+
+	return (val);
+}
