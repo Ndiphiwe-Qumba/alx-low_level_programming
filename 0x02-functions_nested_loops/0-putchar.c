65c4a38fc5695e5d4a5c3de5611e8cdbd6118f64
Ndiphiwe-Qumba <Ndiphiwequmba.nq@gmail.com>
2022-09-14 12:51:33 -0700

    $msg ....Update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "../../main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ * Comment => Betty Style goes here
+ * This program prints _putchar which exist in the header file ../../main.h
+ * Nice Nice  Nice
+ * Returns 0 for Success
+*/
+
 int main(void)
 {
-        puts("\"_\putchar\n");
-        return (0);
+	int i;
+	char c[9] = "_putchar";
+        for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	
+	return (0);
 }
