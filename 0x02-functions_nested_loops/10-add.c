8fd4a76f82838e1e90b159b5edc11bbb1308cd2e
Ndiphiwe-Qumba <Ndiphiwequmba.nq@gmail.com>
2022-09-15 13:01:30 -0700

    $msg ....Update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function adds two ints
+ * @a: first number
+ * @b: second number
+ *
+ * Returns : Addition of two nums
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
