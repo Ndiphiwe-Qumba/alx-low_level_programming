c64bce9dfa89fcbaef7ef456040456896f58f5dc
Ndiphiwe-Qumba <Ndiphiwequmba.nq@gmail.com>
2022-09-15 12:34:01 -0700

    $msg ....Update

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints minutes function
+ *
+ * Returns: 0 or null function returns nothing
+ */
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (H = 0; H < 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
