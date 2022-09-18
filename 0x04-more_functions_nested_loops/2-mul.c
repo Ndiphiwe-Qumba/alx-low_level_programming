8c886bc5082e80228aa7a3ded4447e9bd6caef36
Ndiphiwe-Qumba <Ndiphiwequmba.nq@gmail.com>
2022-09-18 15:43:08 -0700

    $msg ....Update

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,8 +2,8 @@
 
 /**
  * mul - a function that multiplies two integers.
- * @a: An input number
- * @b: An input number
+ * @x: input number
+ * @y: input number
  * Return: Always 0
  */
 int mul(int x, int y)
