40ee1dee2d99c63ccf6eec415b47d4530aa63cb8
Ndiphiwe-Qumba <Ndiphiwequmba.nq@gmail.com>
2022-09-18 16:02:28 -0700

    $msg ....Update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-   * more_numbers - prints 10 times the numbers, from 0 to 14,
-   * 
-   * Return: Always 0
- */
+* more_numbers - prints 10 times the numbers, from 0 to 14 followed by new line
+*
+* Return: Always 0
+*/
 void more_numbers(void)
 {
 	int i, j = 0;
