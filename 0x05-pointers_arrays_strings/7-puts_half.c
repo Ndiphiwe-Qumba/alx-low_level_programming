a8c663e0e81ae0988d930e9f66edeb51826c9e6d
Ndiphiwe-Qumba <Ndiphiwequmba.nq@gmail.com>
2022-09-20 13:55:05 -0700

    $msg ....Update

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - halft of string
+ * @str: string to print
+ *
+ * Return: 0 Success
+ */
+void puts_half(char *str)
+{
+	int len = 0, i, n;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
