f4743c2cae00d8cd6877f9ded0b5b2d720d3432d
Ndiphiwe-Qumba <Ndiphiwequmba.nq@gmail.com>
2022-09-21 15:07:02 -0700

    $msg ....Update

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints char as squarea
+ * @size: size of chars
+ * 
+ * Return: :) Success
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
