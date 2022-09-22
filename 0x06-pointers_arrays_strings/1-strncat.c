cebab201b6da9bc8ba3ad4aaa436646c7e096f1d
Ndiphiwe-Qumba <Ndiphiwequmba.nq@gmail.com>
2022-09-22 14:37:22 -0700

    $msg ....Update

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenates two strings.
+ * 
+ * @dest:  string dest string
+ * @src:   string source string
+ * @n:     number number
+ *
+ * Return: nothing 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
+	while (*dest)
+		dest++;
+
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
+}
