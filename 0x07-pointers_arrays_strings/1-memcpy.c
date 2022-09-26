1feb73f4a92985ae0dd39a66a4432eb8177e262a
Ndiphiwe-Qumba <Ndiphiwequmba.nq@gmail.com>
2022-09-26 15:56:45 -0700

    Fix 5

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: memory area __to
+ * @src:  memory area __from
+ * @n: Number of Bytes
+ *
+ * Return: 0 :) Pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+}
