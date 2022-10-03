54068ac20e935ab49d8a9dc8be30756b4d5997ad
Ndiphiwe-Qumba <NdiphiweQumba.nq@gmail.com>
2022-10-03 13:03:51 -0700

    $msg ....Update

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s1: INput string
+ * @s2: input string 2nd
+ *
+ * Return: Nothing ot Zero
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1 && !*s2)
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+		return (1);
+	if (*s2 == '*' && *(s1 + 1) && *s2)
+		return (0);
+	return (0);
+}
