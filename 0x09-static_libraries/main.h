3af5c51886af61ca3dbd348bc938c2bcb00739e8
Ndiphiwe-Qumba <NdiphiweQumba.nq@gmail.com>
2022-12-03 14:05:25 -0800

    Static l

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -10,7 +10,7 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -18,26 +18,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-
-
-
-
-int check_seperators(char c);
-int _putchar(char c);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-char *_strncat(char *dest, char *src, int n);
-void reverse_array(int *a, int n);
-char *_strcat(char *dest, char *src);
-char *string_toupper(char *);
-char *cap_string(char *);
-void print_buffer(char *b, int size);
-void print_number(int n);
-char *leet(char *);
-char *rot13(char *);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
-int base10(int n);
-
